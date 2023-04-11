b624ace53f27ffc854f34fca44a89cc8630ebe68
William Linhares de Mesquita <128155578+Williamlm20@users.noreply.github.com>
2023-04-11 23:21:40 +0000

    fizz buzz consertado tentativa 2

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ numero = input('digite um numero: ')
 numero = int(numero)
 
 if numero % 3 == 0 and numero % 5 == 0:
-    print('FizzBuzz')
-elif numero % 3 == 0:
-    print('Fizz')
-elif numero % 5 == 0:
-    print('Buzz')
+    print("FizzBuzz")
+
 else:
-    print(numero)
+    if numero % 3 == 0:
+        print("Fizz")
+    elif numero % 5 == 0:
+        print("Buzz")
+    else:
+        print(numero)
