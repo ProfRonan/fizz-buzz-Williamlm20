ef711e97c5fe361194c941731c910d8012b05c74
William Linhares de Mesquita <128155578+Williamlm20@users.noreply.github.com>
2023-04-11 19:01:32 -0300

    Update main.py

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ if numero % 3 == 0 and numero % 5 == 0:
     print('FizzBuzz')
 
 else:
-    if numero % 3 == 0:
+    elif numero % 3 == 0:
         print('Fizz')
-    if numero % 5 == 0:
+    elif numero % 5 == 0:
         print('Buzz')
     else:
         print(numero)
